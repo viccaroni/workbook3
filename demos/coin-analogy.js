8968d7f7c6fa4caa388869d172a85d8510c97358
Victoria Hernandez <v11jhernandez@gmail.com>
2023-05-01 13:10:39 -0400

    coin analogy

---
+++ demos/coin-analogy.js
@@ -0,0 +1,13 @@
+function vendingMachine(coinSlot) {
+  if (coinSlot == "Quarter") {
+    console.log("Crunch");
+  }else{
+    console.log("Coin is returned.");
+  }
+  
+}
+let firstCoin = "Nickel";
+let secondCoin = "Quarter";
+vendingMachine(firstCoin);
+vendingMachine(secondCoin);
+
