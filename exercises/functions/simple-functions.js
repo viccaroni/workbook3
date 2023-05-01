770f3b74f8b74a5b3742e962763637801964ce45
Victoria Hernandez <v11jhernandez@gmail.com>
2023-05-01 11:37:03 -0400

    two functions

---
+++ exercises/functions/simple-functions.js
@@ -0,0 +1,11 @@
+function favouriteThings(){
+console.log("Vic's favourite things are: ");
+console.log("My favourite movie is How to Train your Dragon.");
+console.log("My favourite band is whatever im listening to at the moment.");
+}
+favouriteThings();
+
+function whyImHere(){
+    console.log("My why to being in this program is the massive opportunity that i'm being given. ")
+}
+whyImHere()
