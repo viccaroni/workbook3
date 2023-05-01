d7399bec03d375b96909f8cb719ff97787545e50
Victoria Hernandez <v11jhernandez@gmail.com>
2023-05-01 11:38:54 -0400

    putting backticks instead of double quotes

--- exercises/functions/simple-functions.js
+++ exercises/functions/simple-functions.js
@@ -6,6 +6,6 @@ console.log("My favourite band is whatever im listening to at the moment.");
 favouriteThings();
 
 function whyImHere(){
-    console.log("My why to being in this program is the massive opportunity that i'm being given. ")
+    console.log(`My why to being in this program is the massive opportunity that i'm being given. `)
 }
 whyImHere()
