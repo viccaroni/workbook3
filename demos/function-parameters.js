20275912645bd542c7c511bcf3707b3f0e07743d
Victoria Hernandez <v11jhernandez@gmail.com>
2023-05-03 09:12:11 -0400

    learning

--- demos/function-parameters.js
+++ demos/function-parameters.js
@@ -1,27 +1,3 @@
-// //function recieves the parameter
-// function print(message) {
-//     console.log(message);
-// }
-// //call function pass argument
-// print("A given message");
-
-// //could be reused many times
-// print("helloooo");
-// print("this is very interesting and fun");
-
-// let myMessage = "test this out";
-// print(myMessage);
-
-function shoot(ball) {
-  if (ball == "basketball") {
-    console.log("Score! 3-pointer");
-  } else {
-    console.log("Fail, no score");
-  }
-}
-let basketball = "basketball";
-shoot(basketball);
-
 //function receives the parameter
 function print(message) {
   console.log(message);
@@ -45,3 +21,7 @@ function shoot(ball) {
 
 let basketball = "basketball";
 shoot(basketball);
+
+
+
+
